0915ba4f7471962deff4431d0590f36d53662977
suzuki-MF <suzuki_honoka@mindfree.co.jp>
2024-06-24 11:48:10 +0900

    useFetch オプション削除

--- src/pages/api/index.vue
+++ src/pages/api/index.vue
@@ -1,21 +1,19 @@
 <template>
-  <ClientOnly>
-    <v-container class="pa-6 pa-md-12">
-      <h2>API接続テスト</h2>
-      <v-sheet class="mx-auto" width="300">
-        <v-list lines="two">
-          <v-list-item
-            v-for="n in EventResponse.list"
-            :key="n"
-            :title="'title ' + n.subject"
-            subtitle="Load check ok"
-          ></v-list-item>
-        </v-list>
-      </v-sheet>
+  <v-container class="pa-6 pa-md-12">
+    <h2>API接続テスト</h2>
+    <v-sheet class="mx-auto" width="300">
+      <v-list lines="two">
+        <v-list-item
+          v-for="n in EventResponse.list"
+          :key="n"
+          :title="'title ' + n.subject"
+          subtitle="Load check ok"
+        ></v-list-item>
+      </v-list>
+    </v-sheet>
 
-      <v-btn variant="tonal" @click="goBack"> ← Back </v-btn>
-    </v-container>
-  </ClientOnly>
+    <v-btn variant="tonal" @click="goBack"> ← Back </v-btn>
+  </v-container>
 </template>
 
 <script setup lang="ts">
@@ -24,7 +22,7 @@ import { useRoute, useRouter } from "vue-router";
 const config = useRuntimeConfig();
 const { data: EventResponse } = await useFetch(
   `${config.public.kurocoApiDomain}/rcms-api/1/sample`,
-  { credentials: "include", server: false }
+  { credentials: "include" }
 );
 
 const route = useRoute();
